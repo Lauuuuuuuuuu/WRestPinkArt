097e1dfcc7f98d6dbb7f0656d58d60a2a916904a
nataliaverano022 <93163936+nataliaverano022@users.noreply.github.com>
2022-04-25 22:14:34 -0500

    initial commit

---
+++ src/main/java/co/edu/unbosque/wrestpinkart/dtos/ObrasArte.java
@@ -0,0 +1,49 @@
+package co.edu.unbosque.wrestpinkart.dtos;
+
+import java.io.File;
+
+public class ObrasArte {
+
+    private String titulo;
+    private int precio;
+    private File imagen;
+
+    public ObrasArte(String t, int p, File i){
+        this.imagen = i;
+        this.precio = p;
+        this.titulo=t;
+    }
+
+    public File getImagen() {
+        return imagen;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setImagen(File imagen) {
+        this.imagen = imagen;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "DatosObrasArte{" +
+                "titulo='" + titulo + '\'' +
+                ", precio=" + precio +
+                ", imagen=" + imagen +
+                '}';
+    }
+}
---
+++ src/main/java/co/edu/unbosque/wrestpinkart/resources/UsersResource.java
@@ -0,0 +1,84 @@
+package co.edu.unbosque.wrestpinkart.resources;
+
+import co.edu.unbosque.wrestpinkart.dtos.ExceptionMessage;
+import co.edu.unbosque.wrestpinkart.dtos.Usuario;
+import co.edu.unbosque.wrestpinkart.services.AgregarUsuario;
+import jakarta.servlet.ServletContext;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Path("/users")
+public class UsersResource {
+
+    @Context
+    ServletContext context;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response list() {
+        try {
+            List<Usuario> usuarios = new AgregarUsuario().getUsers();
+
+            return Response.ok()
+                    .entity(usuarios)
+                    .build();
+        } catch (IOException e) {
+            return Response.serverError().build();
+        }
+    }
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createForm(
+            @FormParam("username") String username,
+            @FormParam("password") String password,
+            @FormParam("role") String role
+    ) {
+        String contextPath =context.getRealPath("") + File.separator;
+
+        try {
+            Usuario usuario = new AgregarUsuario().crearUsuario(username, password, role, "0",contextPath);
+
+            return Response.created(UriBuilder.fromResource(UsersResource.class).path(username).build())
+                    .entity(usuario)
+                    .build();
+        } catch (IOException e) {
+            return Response.serverError().build();
+        }
+    }
+
+    @GET
+    @Path("/{username}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@PathParam("username") String username) {
+        try {
+            List<Usuario> users = new AgregarUsuario().getUsers();
+
+            Usuario usuario = users.stream()
+                    .filter(u -> u.getUsername().equals(username))
+                    .findFirst()
+                    .orElse(null);
+
+            if (usuario != null) {
+                return Response.ok()
+                        .entity(usuario)
+                        .build();
+            } else {
+                return Response.status(404)
+                        .entity(new ExceptionMessage(404, "User not found"))
+                        .build();
+            }
+        } catch (IOException e) {
+            return Response.serverError().build();
+        }
+    }
+
+
+}
